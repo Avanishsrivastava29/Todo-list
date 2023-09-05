84b08b75c082031940a4507d47994b8f9e585131
Avanish srivastava <avanishsrivastava78@gmail.com>
2023-09-05 15:41:37 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [tasks, setTasks] = useState([]);
+  const [taskText, setTaskText] = useState('');
+
+  const addTask = () => {
+    if (taskText.trim() !== '') {
+      setTasks([...tasks, taskText]);
+      setTaskText('');
+    }
+  };
+
+  const removeTask = (index) => {
+    const updatedTasks = tasks.filter((_, i) => i !== index);
+    setTasks(updatedTasks);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="app-container"> {/* Added a container div */}
+        <h1>To-Do App</h1>
+        <div className="task-input">
+          <input
+            type="text"
+            placeholder="Add a new task..."
+            value={taskText}
+            onChange={(e) => setTaskText(e.target.value)}
+          />
+          <button onClick={addTask}>Add</button>
+        </div>
+        <div className="task-list">
+          {tasks.map((task, index) => (
+            <div
+              className="task-item"
+              key={index}
+              style={{
+                background: `linear-gradient(135deg, #ff6b6b, #8c7ae6)`,
+              }}
+            >
+              <span>{task}</span>
+              <button onClick={() => removeTask(index)}>Remove</button>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
